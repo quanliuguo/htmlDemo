648f040be8ffd1c276426476f25d673b3d81e329
quanliuguo <1220710037@qq.com>
2020-08-27 01:14:18 +0800

    tijiao

--- ZZthis/index.js
+++ ZZthis/index.js
@@ -40,3 +40,65 @@ var name = 'NAME';
 
 fn(); 
 fn1() */
+
+/* 开始 */
+/* ;(function() {
+    function identify() {
+        console.log('identify->', this.name)
+        return this.name.toUpperCase()
+    }
+    function speak() {
+        console.log('speak->', this)
+        var greeting = "Hello, I am " + identify.call(this) 
+        console.log(greeting)
+    }
+    var me = {
+        name: 'Kyle'
+    }
+    var you = {
+        name: 'Xuebi'
+    }
+    
+    identify.call(me)
+    identify.call(you)
+    
+    speak.call(me)
+    speak.call(you)
+})(); */
+
+
+/* ;(function() {
+    // this 是否指向自身
+    function foo(i) {
+        console.log('i', i)
+        this.count++
+    }
+    
+    foo.count = 0
+    // console.dir(foo)
+    var i;
+    for(i=0; i<10; i++) {
+        if(i>5) {
+            foo(i)
+        }
+    }
+    
+    console.log( 'foo.count->', foo.count)
+}()); */
+
+;(function(){
+    // 
+    function foo() {
+        console.log(this)
+        this.count = 4
+        console.dir(foo)
+    }
+    
+    // setTimeout(foo, 4000)
+    foo()
+}());
+
+
+// ;(function(){
+//     console.log(this)
+// }());
